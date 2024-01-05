58a3571e199322608a5aa87b3b6118047ffe68b0
Guilherme de Freitas <guilherme.de-freitas@diamond.ac.uk>
2024-01-05 11:53:59 +0000

    Remove unused imports

--- tests/proposals/test_get_data.py
+++ tests/proposals/test_get_data.py
@@ -1,21 +1,15 @@
 import json
 
 import responses
-from sqlalchemy import select
 
-from sample_handling.models.inner_db.tables import Shipment
 from sample_handling.utils.config import Config
-from sample_handling.utils.database import inner_db
-from sample_handling.utils.external import Expeye
 
 
 @responses.activate
 def test_get(client):
     """Should get proposal metadata"""
-    patch_resp = responses.get(
-        f"{Config.ispyb_api}/proposals/cm00001/data", json.dumps({})
-    )
+    resp = responses.get(f"{Config.ispyb_api}/proposals/cm00001/data", json.dumps({}))
 
     client.get("/proposals/cm00001/data")
 
-    assert patch_resp.call_count == 1
+    assert resp.call_count == 1
