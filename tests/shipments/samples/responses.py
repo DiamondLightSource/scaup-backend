95e5b6fa2b9285fa3a99e5702a1efd0c60607bf1
Guilherme de Freitas <guilherme.de-freitas@diamond.ac.uk>
2024-04-22 14:29:12 +0100

    Prefix macromolecule to sample name

--- tests/shipments/samples/responses.py
+++ tests/shipments/samples/responses.py
@@ -2,10 +2,13 @@ import json
 
 
 def protein_callback(request):
-    # Return valid response for proteinId 1, return 404 for all else
+    # Return valid response for proteinId 4407 and 5000, return 404 for all else
     protein_id = request.path_url.split("/")[2]
 
     if protein_id == "4407":
         return (200, {}, json.dumps({"name": "Protein_01"}))
+
+    if protein_id == "5000":
+        return (200, {}, json.dumps({"name": "-nv^]id name"}))
 
     return (404, {}, "")
