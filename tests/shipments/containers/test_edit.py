35a6f7a57d1f739de41f480f84c4ad62fc0bb99e
Guilherme de Freitas <guilherme.de-freitas@diamond.ac.uk>
2024-07-17 09:29:11 +0100

    Update test

--- tests/shipments/containers/test_edit.py
+++ tests/shipments/containers/test_edit.py
@@ -39,8 +39,10 @@ def test_edit_inexistent_sample(client):
     assert resp.status_code == 404
 
 
+@responses.activate
 def test_update_samples_on_shipment_id_change(client):
     """Should update shipment ID for children if shipment ID of parent is updated"""
+    responses.patch(f"{Config.ispyb_api}/sample-handling/containers/303613", "{}")
 
     resp = client.patch(
         "/containers/776",
