a2d88a90754f06c0cfd1a931710b77d987a1eb85
Guilherme de Freitas <guilherme.de-freitas@diamond.ac.uk>
2024-10-03 13:09:07 +0100

    Add tests

--- tests/shipments/test_tracking_labels.py
+++ tests/shipments/test_tracking_labels.py
@@ -15,3 +15,11 @@ def test_no_bar_code(client):
     resp = client.get("/shipments/2/tracking-labels")
 
     assert resp.status_code == 404
+
+
+@responses.activate
+def test_no_top_level_containers(client):
+    """Should return 404 if shipment has no top level containers"""
+    resp = client.get("/shipments/118/tracking-labels")
+
+    assert resp.status_code == 404
