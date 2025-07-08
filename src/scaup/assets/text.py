97ee0b5cbc2c0a73993033e2e4676d8ff79d46ec
Guilherme Francisco <guilherme.de-freitas@diamond.ac.uk>
2025-07-08 16:53:26 +0100

    Render temporary barcodes for dewars (#6)

--- src/scaup/assets/text.py
+++ src/scaup/assets/text.py
@@ -1,6 +1,10 @@
 LABEL_INSTRUCTIONS_STEP_1 = (
     "Print tracking labels, and affix one of them to the dewar. If you're sending multiple "
     + "dewars, ensure the dewar code on the label matches the dewar code on the dewar."
+    + "\n\nIf a dewar doesn't have a barcode (such as the ones on the last page) already, cut out "
+    + "the barcode on the last page and affix it to the dewar. If you're sending multiple dewars, ensure "
+    + "the dewar matches the barcode you've selected for it previously. \n\nIgnore the last page if "
+    + "you already have barcodes on your dewars."
 )
 
 LABEL_INSTRUCTIONS_STEP_2 = "Affix the other label to the dewar case"
@@ -15,4 +19,8 @@ LABEL_INSTRUCTIONS_STEP_4 = (
     "Once your session is finished, you must request for your dewars to be returned if you wish for them to be "
     + "returned to your institution. \nWhen you receive your dewar, feel free to discard any labels from the "
     + "previous steps."
+)
+
+TEMPORARY_DEWAR_TEXT = (
+    "FOR STAFF: This is a temporary barcode. Replace it with a permanent sticker before the end of the visit."
 )
